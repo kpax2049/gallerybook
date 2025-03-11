39876378048801197558bf301ecde821623914a2
kpax2049 <kpax2049@gmail.com>
2025-03-11 09:50:12 +0100

    Beginning setup for Gallery comments; current progress on gallery editor

--- frontend/src/app/gallery/Gallery.tsx
+++ frontend/src/app/gallery/Gallery.tsx
@@ -2,15 +2,12 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Gallery, getGallery } from '@/api/gallery';
 import { TipTapRenderer } from '@/components/ui/TiptapRender';
-
-// type GalleryProps = {
-//   children?: ReactNode;
-// };
+import CommentPanel from './galleryComment/Comment';
 
 export default function GalleryPage() {
   // { children }: GalleryProps
   const [gallery, setGallery] = useState<Gallery>();
-  const [content, setContent] = useState<string>('');
+  const [content, setContent] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
   const { galleryId } = useParams();
   useEffect(() => {
@@ -28,7 +25,10 @@ export default function GalleryPage() {
     <div className="grid auto-rows-min gap-4 p-4 justify-between">
       <h3>Gallery Item: {galleryId}</h3>
       {content && content.length > 0 && (
-        <TipTapRenderer {...{ content: content }} />
+        <div>
+          <TipTapRenderer {...{ content: content }} />
+          <CommentPanel />
+        </div>
       )}
     </div>
   );
